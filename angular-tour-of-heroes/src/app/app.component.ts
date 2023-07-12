44af1bf757be1313a9ed51db41566b84be820c1e
Marcel-Steven Felix <marcel-steven.felix@accenture.com>
2023-07-12 16:38:00 +0200

    remove logout function from app.component and implement logout component in html

--- angular-tour-of-heroes/src/app/app.component.ts
+++ angular-tour-of-heroes/src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from './auth/auth.service';
 import { Observable, map } from 'rxjs';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -14,19 +13,10 @@ export class AppComponent {
 
   constructor(
     private authService: AuthService,
-    private router: Router,
+
   ) {}
   
   isLoggedIn() {
     return this.authService.isAuthenticated();
-  }
-  
-  // Logs out user and removes loggedIn Item
-  logout(): void {
-    const currentUrl = window.location.pathname;
-    this.authService.logout();
-    localStorage.removeItem('loggedIn')
-    this.router.navigate(['/accounts/login'], { queryParams: { returnUrl: currentUrl } });
-    
   }
 }
