b92d367f7ffd0b82707d6b154f66b7f3dad4c54d
Marcel-Steven Felix <marcel-steven.felix@accenture.com>
2023-07-12 16:37:10 +0200

    move logout function into logout component and style it with css

--- angular-tour-of-heroes/src/app/logout/logout.component.ts
+++ angular-tour-of-heroes/src/app/logout/logout.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -9,5 +10,20 @@ import { AuthService } from '../auth/auth.service';
 })
 export class LogoutComponent {
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    
+    ) {}
+
+  // Logs out user and removes loggedIn Item
+  logout(): void {
+    const currentUrl = window.location.pathname;  
+    this.authService.logout().subscribe(() => {
+      localStorage.removeItem('loggedIn');
+      this.router.navigate(['/accounts/login'], { queryParams: { returnUrl: currentUrl } });
+    });
+  }
+  
+
 }
